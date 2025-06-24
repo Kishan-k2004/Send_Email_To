ea3d4e4afefc35f60927f0ef139b9fd3e64fc5dc
Kishan-k2004 <k.khansali.2004@gmail.com>
2025-06-24 12:15:28 +0530

    added cors link

--- main.js
+++ main.js
@@ -7,7 +7,11 @@ const cors = require("cors");
 
 const app = express();
 app.use(express.json());
-app.use(cors());
+app.use(cors({
+  origin : 'https://bloggify-one.vercel.app',
+  methods : ['GET','POST'],
+  credentials : true
+}));
 
 const otpStore = {}; // { email: { otp: "123456", expiresAt: timestamp } }
 
